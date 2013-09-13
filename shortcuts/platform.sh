776c108ef79bd76da274b060593a4494a5028e77
MTM <a-matsumoto@roboken.esys.tsukuba.ac.jp>
2013-09-13 13:18:27 +0900

    複数ショートカットを同時起動した際にterminatorコンフィグが戻らなくなるバグを修正

--- shortcuts/platform.sh
+++ shortcuts/platform.sh
@@ -1,20 +1,40 @@
 #!/bin/bash
 
 #==============================================
-#  yp-spurの起動
+#  yp-spur,ssmの起動
 #==============================================
-
 
 shell_home=${PWD}
 cd ../
 export tamago_home=${PWD}
 cd $shell_home
 
+#コンフィグディレクトリがないなら、作る
 if [ ! -d ~/.config/terminator ] ; then
 	mkdir ~/.config/terminator
 fi
 
-mv ~/.config/terminator/config ~/.config/terminator/config.bk
-cp terminator-config ~/.config/terminator/config
+#すでに他のショートカットによってバックアップファイルが作成されているか？
+if [ ! -f ~/.config/terminator/config.bk ] ; then
+	make_buckup="1"
+	echo "make TAMAGO terminator configure file."
+else
+	make_buckup="0"
+	echo "TAMAGO terminator configure file already exists."
+fi
+
+
+#バックアップを作る(バックアップがまだない場合)
+if [ ${make_buckup} = "1" ] ; then
+	mv ~/.config/terminator/config ~/.config/terminator/config.bk
+	cp terminator-config ~/.config/terminator/config
+fi
+
+#実行
 terminator -l platform
-mv ~/.config/terminator/config.bk ~/.config/terminator/config	
+
+#バックアップを自分で作ったときは、バックアップから元のコンフィグを復元
+if [ ${make_buckup} = "1" ] ; then
+	mv ~/.config/terminator/config.bk ~/.config/terminator/config	
+	echo "reconstituted original terminator configure file."	
+fi
--- shortcuts/urg.sh
+++ shortcuts/urg.sh
@@ -4,13 +4,37 @@
 # urg-proxyとls-coordinate-converterの起動
 #==============================================
 
-
 shell_home=${PWD}
 cd ../
 export tamago_home=${PWD}
 cd $shell_home
 
-mv ~/.config/terminator/config ~/.config/terminator/config.bk
-cp terminator-config ~/.config/terminator/config
+#コンフィグディレクトリがないなら、作る
+if [ ! -d ~/.config/terminator ] ; then
+	mkdir ~/.config/terminator
+fi
+
+#すでに他のショートカットによってバックアップファイルが作成されているか？
+if [ ! -f ~/.config/terminator/config.bk ] ; then
+	make_buckup="1"
+	echo "make TAMAGO terminator configure file."
+else
+	make_buckup="0"
+	echo "TAMAGO terminator configure file already exists."
+fi
+
+
+#バックアップを作る(バックアップがまだない場合)
+if [ ${make_buckup} = "1" ] ; then
+	mv ~/.config/terminator/config ~/.config/terminator/config.bk
+	cp terminator-config ~/.config/terminator/config
+fi
+
+#実行
 terminator -l urg
-mv ~/.config/terminator/config.bk ~/.config/terminator/config	
+
+#バックアップを自分で作ったときは、バックアップから元のコンフィグを復元
+if [ ${make_buckup} = "1" ] ; then
+	mv ~/.config/terminator/config.bk ~/.config/terminator/config	
+	echo "reconstituted original terminator configure file."	
+fi
