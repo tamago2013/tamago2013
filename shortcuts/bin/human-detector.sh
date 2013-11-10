c51d7a8fd74b210f877f10058a83f93119d1602b
MTM <a-matsumoto@roboken.esys.tsukuba.ac.jp>
2013-11-10 18:52:18 +0900

    modified shortcuts

---
+++ shortcuts/bin/human-detector.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+#==============================================
+# human-detectorの起動
+#==============================================
+
+#tamagoディレクトリホームパスの取得
+if [ ! -f ~/.config/tamago/home-path.sh ] ; then
+	echo "cannot find \"~/.config/tamago/home-path.sh\" !!"
+	cd ../../
+ 	tamago_home=${PWD}
+else
+ . ~/.config/tamago/home-path.sh  
+fi
+
+cd ${tamago_home}/shortcuts
+export tamago_home
+
+#コンフィグディレクトリがないなら、作る
+if [ ! -d ~/.config/terminator ] ; then
+	mkdir ~/.config/terminator
+fi
+
+#すでに他のショートカットによってバックアップファイルが作成されているか？
+if [ ! -f ~/.config/terminator/config.bk ] ; then
+	make_buckup="1"
+	echo "make TAMAGO terminator configure file."
+else
+	make_buckup="0"
+	echo "TAMAGO terminator configure file already exists."
+fi
+
+
+#バックアップを作る(バックアップがまだない場合)
+if [ ${make_buckup} = "1" ] ; then
+	mv ~/.config/terminator/config ~/.config/terminator/config.bk
+	cp ./conf/terminator-config ~/.config/terminator/config
+fi
+
+#実行
+terminator -l human-detector -T human-detector
+
+#バックアップを自分で作ったときは、バックアップから元のコンフィグを復元
+if [ ${make_buckup} = "1" ] ; then
+	mv ~/.config/terminator/config.bk ~/.config/terminator/config	
+	echo "reconstituted original terminator configure file."	
+fi
--- shortcuts/uninstall.sh
+++ shortcuts/uninstall.sh
@@ -4,15 +4,21 @@
 # デスクトップランチャとその関係ファイルを削除
 #================================
 
+#現役のランチャ名
 rm -f ~/Desktop/joystick.desktop
 rm -f ~/Desktop/localizer.desktop
 rm -f ~/Desktop/multi-logger.desktop
-rm -f ~/Desktop/path-planner.desktop
+rm -f ~/Desktop/main-controller.desktop
+rm -f ~/Desktop/human-detector.desktop
 rm -f ~/Desktop/platform.desktop
 rm -f ~/Desktop/ssm-killer.desktop
 rm -f ~/Desktop/urg.desktop
 rm -f ~/Desktop/visualizer.desktop
 rm -f ~/Desktop/ptz.desktop
 
+#古いランチャ名
+rm -f ~/Desktop/path-planner.desktop
+
+#tamagoディレクトリを削除
 rm -fr ~/.config/tamago
 
